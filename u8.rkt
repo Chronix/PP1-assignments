0a629000b6507ffe7563fbd673db33513723c767
Chronix <chronix88@gmail.com>
2010-11-29 21:52:25 +0100

    update - home

---
+++ u8.rkt
@@ -0,0 +1,11 @@
+(define set-diff
+  (lambda (s1 s2)
+    (cond
+      ((null? s1) '())
+      ((member (car s1) s2) (set-diff (cdr s1) s2))
+      (else (cons (car s1) (set-diff (cdr s1) s2))))))
+
+(set-diff '(1 2 3) '())
+(set-diff '() '(1 2))
+(set-diff '(1 2 3) '(1 3 5))
+(set-diff '(1 2 3) '(0 2 5 8))
