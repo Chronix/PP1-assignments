cff30ca05dbeafed594464729c675e46214eccdd
Chronix <chronix88@gmail.com>
2010-11-29 22:01:16 +0100

    update - home

--- u8.rkt
+++ u8.rkt
@@ -5,7 +5,8 @@
       ((member (car s1) s2) (set-diff (cdr s1) s2))
       (else (cons (car s1) (set-diff (cdr s1) s2))))))
 
-(set-diff '(1 2 3) '())
-(set-diff '() '(1 2))
-(set-diff '(1 2 3) '(1 3 5))
-(set-diff '(1 2 3) '(0 2 5 8))
+;(set-diff '(1 2 3) '())
+;(set-diff '() '(1 2))
+;(set-diff '(1 2 3) '(1 3 5))
+;(set-diff '(1 2 3) '(0 2 5 8))
+               
